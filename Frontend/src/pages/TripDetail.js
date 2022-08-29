2a891ed860e6cdda2f9d60152f5a54353c36467e
Zhengyu0913 <fanzhengyu1995@outlook.com>
2022-08-29 00:27:20 -0700

    update tripdetail

---
+++ Frontend/src/pages/TripDetail.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TripDetail() {
+  return <div>Trip Detail</div>;
+}
--- Frontend/src/pages/Trips.js
+++ Frontend/src/pages/Trips.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Trips() {
-  return <h1>Trips</h1>;
+  return (
+    <>
+      <h1>Trips</h1>
+      <Link to="/trips/details"> Trip Details</Link>
+    </>
+  );
 }
