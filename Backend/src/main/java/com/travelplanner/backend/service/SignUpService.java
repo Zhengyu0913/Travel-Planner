4e96b991e0425c35311783467d2cfb56005c06a4
Zhengyu Fan <fanzhengyu1995@outlook.com>
2022-09-06 16:14:41 -0700

    update backend

---
+++ Backend/src/main/java/com/travelplanner/backend/service/SignUpService.java
@@ -0,0 +1,21 @@
+package com.travelplanner.backend.service;
+
+import com.travelplanner.backend.dao.SignUpDao;
+import com.travelplanner.backend.entity.User;
+import com.travelplanner.backend.util.Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class SignUpService {
+
+    @Autowired
+    private SignUpDao signUpDao;
+
+    public boolean signUp(User user) throws IOException {
+        user.setPassword(Util.encryptPassword(user.getEmail(), user.getPassword()));
+        return signUpDao.signUp(user);
+    }
+}
